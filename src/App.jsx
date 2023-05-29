3da3fa637d4378ffb12a849ea84e99e77bb946ee
Ignatius andri pratama putra <ignaway@gmail.com>
2023-05-30 00:34:20 +0700

    add react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
+import { Box } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-    hello 
-    </div>
+    <Box bg='tomato' w='100%' p={4} color='white'>
+    This is the Box
+  </Box>
   );
 }
 
