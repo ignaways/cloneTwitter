65c28b244dba429dae37a668ceecbd4bae5ddfc1
Ignatius andri pratama putra <ignaway@gmail.com>
2023-06-01 22:24:14 +0700

    add responsive

--- src/components/container/BodyContainer.jsx
+++ src/components/container/BodyContainer.jsx
@@ -1,7 +1,19 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 const BodyContainer = ({ children }) => {
+  const [isWeb, setIsWeb] = useState(window.innerWidth > 1020);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWeb(window.innerWidth > 1020);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Grid 
@@ -9,16 +21,17 @@ const BodyContainer = ({ children }) => {
         templateColumns="repeat(18, 1fr)"
         gap={8}
       >
+        {console.log(isWeb)}
       {children.map((child, i) => {
           return (
             <Fragment key={i}>
               {i === 0 && (
-                <GridItem rowSpan={0} colSpan={10}>
+                <GridItem rowSpan={0} colSpan={{base:18, md:18, lg:10}}>
                   {child}
                 </GridItem>
               )}
-              {i !== 0 && (
-                <GridItem rowSpan={0} colSpan={6}>
+              {i !== 0 && isWeb && (
+                <GridItem rowSpan={0} colSpan={{lg:7}}>
                   {child}
                 </GridItem>
               )}
--- src/components/container/MainContainer.jsx
+++ src/components/container/MainContainer.jsx
@@ -4,17 +4,17 @@ import { Fragment } from "react";
 const MainContainer = ({ children }) => {
   return (
     <>
-      <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(24, 1fr)">
+      <Grid templateColumns={{base: "repeat(1, 1fr)",md:"repeat(24, 1fr)"}}>
         {children.map((child, i) => {
           return (
             <Fragment key={i}>
               {i === 0 && (
-                <GridItem rowSpan={0} colSpan={6}>
+                <GridItem colSpan={{base:1, md:4, lg:6}}>
                   {child}
                 </GridItem>
               )}
               {i !== 0 && (
-                <GridItem rowSpan={0} colSpan={18}>
+                <GridItem colSpan={{base:1, md:20, lg:18}}>
                   {child}
                 </GridItem>
               )}
